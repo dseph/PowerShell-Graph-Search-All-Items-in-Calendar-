35d2e621f1f8a627c862f755efda24bdaf7a0b84
Daniel Bagley <dseph@users.noreply.github.com>
2026-03-25 18:23:54 -0400

    Create PowerShell-Graph-SearchAllItemsInCalendar_Credentials.ps1

---
+++ PowerShell-Graph-SearchAllItemsInCalendar_Credentials.ps1
@@ -0,0 +1,9 @@
+# PowerShell-Graph-SearchAllItemsInCalendar_Credentials.ps1
+
+# =========================
+# CONFIG (fill these in)
+# =========================
+$TenantId     = "YOUR_TENANT_ID"                # TODO - set
+$ClientId     = "YOUR_CLIENT_ID"                # TODO - set
+$ClientSecret = "YOUR_CLIENT_SECRET"            # TODO - set
+
